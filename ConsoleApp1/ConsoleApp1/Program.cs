e485188d094438903930cd3481e40114611c87fe
KamranG12 <kamran.Jabiyev@aplusa-security.com>
2017-05-04 02:01:47 +0400

    first

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Gelir:"+" "+ netGelir());
+            Console.WriteLine("Faiz:" + " " + faiz());
+            Console.ReadLine();
+        }
+
+        static double brutGelir(ushort a=90,byte b=20)
+        {
+            return a * b;
+        }
+        static double faiz()
+        {
+            if (brutGelir() >= 1000)
+            {
+                return brutGelir() * 0.18;
+            }
+            else
+            {
+                return brutGelir() * 0.04;
+            }
+        }
+        static double netGelir()
+        {
+            return brutGelir() - faiz();
+        }
+    }
+}
---
+++ viza_final/viza_final/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace viza_final
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a;
+            if(netice()>=45)
+            {
+                a="Kechdin";
+              Console.WriteLine(a);
+              Console.ReadLine();
+            }else{
+                a="Qaldin";
+              Console.WriteLine(a);
+              Console.ReadLine();
+            }
+        }
+        static double netice(ushort viza=10,ushort final=200)
+            {
+              return (viza*0.4)+(final*0.6);
+            }
+    }
+}
