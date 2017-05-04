be991839b695897cd6f94ddbc4fa6d5e5bdeb498
KamranG12 <kamran.jabiyev@aplusa-security.com>
2017-05-04 11:52:50 +0400

    uch_dort_sonda_2ci_alqoritm

---
+++ kub_kvadrat/kub_kvadrat/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kub_kvadrat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Eded daxil et:");
+            Console.WriteLine(kvadratKub());
+            Console.ReadLine();
+        }
+        static int kvadratKub()
+        {
+            int result;
+            int eded = Convert.ToInt32(Console.ReadLine());
+            if (eded % 2 == 0)
+            {
+                result = eded * eded;
+            }
+            else
+            {
+                result = eded * eded * eded;
+            }
+            return result;
+
+        }
+    }
+}
